c54eb88ff5551130b68a73bd14624db73472649a
yarov475 <59488110+yarov475@users.noreply.github.com>
2021-04-10 01:00:17 +0300

    crud update throw $_POST

--- vendor/update.php
+++ vendor/update.php
@@ -3,6 +3,6 @@ require_once '../config/connect.php';
 $title = $_POST['title'];
 $price= $_POST['price'];
 $description = $_POST['Description'];
-$id = $_GET['id'];
+$id =$_POST['id'];
 mysqli_query($connect, "UPDATE `products` SET `title` = '$title', `price` = '$price', `description` = '$description' WHERE `products`.`id` = $id");
 header('location: ../index.php');
